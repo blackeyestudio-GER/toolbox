61f1d6576875ec21a40b31064033eef5018dc23e
arthurk <arthur.kielbasa@gmail.com>
2025-11-05 23:47:21 +0100

    change adds to maybe work again

--- components/AdBannerBottom.vue
+++ components/AdBannerBottom.vue
@@ -1,12 +1,4 @@
 <template>
-  <!--div class="w-full h-80 flex justify-center mt-8 min-h-32 min-w-full">
-        <Adsbygoogle
-            ad-slot="5443827893"
-            analytics-domain-name="blackeyestudio.de"
-            class="w-full h-80 flex justify-center mt-8 min-h-32 min-w-full"
-        />
-    </div-->
-    <!-- werbung unten -->
     <ClientOnly>
         <div class="w-full flex justify-center mt-8 min-h-32 min-w-full">
             <ins class="adsbygoogle"
